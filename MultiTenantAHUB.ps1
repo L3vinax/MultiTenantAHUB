00f7a43c589ee3fadffd88dd428edff8bf14e23f
Aaron Hutchinson <111082932+L3vinax@users.noreply.github.com>
2024-04-04 09:19:56 -0700

    Update MultiTenantAHUB.ps1

--- MultiTenantAHUB.ps1
+++ MultiTenantAHUB.ps1
@@ -1,17 +1,13 @@
 #Connect-AzAccount
-
-$aztenants = Get-AzTenant
-foreach ($aztenant in $aztenants){
-$aztenantid = $aztenant.id
-Set-AzContext -TenantId $aztenantid
-
 $azSubs = Get-AzSubscription
+
+$AzureVM = @()
 
 foreach ($azSub in $azSubs){
     Set-AzContext -Subscription $azsub | Out-Null
-    $azsubname = $azsub.Name
-    $AzureVM = @()
-    $AzureVMSS = @()
+    $tenantid = (Get-AzContext).Tenant.Id
+    $tenantName = (Get-azTenant | where-object Id -eq $tenantid).Name
+
 
     foreach ($azVM in Get-AzVM){
         $sku = get-azvmsize  -vmname $azVM.Name -ResourceGroupName $azVM.ResourceGroupName | where {$_.Name -eq $azVM.HardwareProfile.VmSize}
@@ -23,8 +19,8 @@ foreach ($azSub in $azSubs){
         }
         
         $results = @{
-            Tenant = $aztenantname
-            Subscription = $azsubname
+            Tenant = $tenantName
+            Subscription = $azsub.Name
             VMName = $azVM.Name
             Cores = $cores
             
@@ -44,36 +40,5 @@ foreach ($azSub in $azSubs){
         $AzureVM += $ServiceObject
     }
     
-    foreach ($vmss in Get-Azvmss) {
-    
-    $vmsssku = get-azvmsize -Location $vmss.Location | where {$_.Name -eq $vmss.sku.Name}
-    if ($vmsssku.NumberOfCores -lt 8){
-    $vmsscores = 8
-    }
-    else {
-    $vmsscores = $vmsssku.NumberOfCores
-    }
-    
-    $vmssresults = @{
-        Tenant = $aztenantname
-        Subscription = $azsubname
-        VMSS = $vmss.Name
-        Cores = $vmsscores
-        }
-        if (!$vmss.VirtualMachineProfile.LicenseType) {
-            $vmssresults += @{
-                LicenseType = "No License"
-            }
-        }
-        else {
-            $vmssresults += @{
-                LicenseType = $vmss.VirtualMachineProfile.LicenseType
-            }
-        }
-        $AzureVMSS += $ServiceObject
-
-}
+}    
     $AzureVM | Export-CSV -Path "c:\temp\AzVM-Licensing.csv" -NoTypeInformation -Force
-
-}
-}
